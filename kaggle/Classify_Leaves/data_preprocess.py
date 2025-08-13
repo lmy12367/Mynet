3b6d09fb95a81b311f4814a24b51feae4228f8c5
lmy12367 <lfuturemy@163.com>
2025-08-13 21:42:41 +0800

    建立「类别↔索引」双向映射

--- kaggle/Classify_Leaves/data_preprocess.py
+++ kaggle/Classify_Leaves/data_preprocess.py
@@ -1,4 +1,8 @@
+import json
+
 import pandas as pd
+import os
+from sklearn.preprocessing import LabelEncoder
 
 train_df=pd.read_csv("./data/train.csv")
 test_df=pd.read_csv("./data/test.csv")
@@ -7,3 +11,11 @@ print(train_df.head())
 print(len(train_df))
 print((len(test_df)))
 print(train_df['label'].nunique())
+
+le = LabelEncoder()
+train_df['label_id'] = le.fit_transform(train_df['label'])
+num_classes=len(le.classes_)
+print(num_classes)
+
+with open("./data/label2idx.json",'w') as f:
+    json.dump({l:i for i,l in enumerate(le.classes_)},f)
