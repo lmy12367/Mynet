2566d4eecc1c549c1a06309fe45da34aa76f5f1a
lmy12367 <lfuturemy@163.com>
2025-09-05 22:58:22 +0800

    seq model1

--- RNN/Sequence_model/model.py
+++ RNN/Sequence_model/model.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 T=1000
 time=torch.arange(1,T+1,dtype=torch.float32)
 x=torch.sin(0.01*time)+torch.normal(0,0.2,(T,))
+print(x.shape)
 plt.figure(figsize=(6,3))
 plt.plot(time.numpy(), x.numpy())
 plt.title("sin box")
@@ -12,3 +13,13 @@ plt.xlabel("time")
 plt.ylabel("x")
 plt.show()
 
+tau=4
+
+features=torch.zeros((T-tau,tau))
+print(features.shape)
+for i in range(tau):
+    features[:,i]=x[i:T-tau+i]
+
+labels=x[tau:].reshape((-1,1))
+print("features形状:", features.shape)  
+print("labels形状:", labels.shape)    
