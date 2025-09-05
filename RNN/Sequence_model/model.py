e4c38d7b8781e01a7ff76bf73b632025acb4243d
lmy12367 <lfuturemy@163.com>
2025-09-05 22:37:25 +0800

    seq model

---
+++ RNN/Sequence_model/model.py
@@ -0,0 +1,14 @@
+import torch
+from torch import nn
+import matplotlib.pyplot as plt
+
+T=1000
+time=torch.arange(1,T+1,dtype=torch.float32)
+x=torch.sin(0.01*time)+torch.normal(0,0.2,(T,))
+plt.figure(figsize=(6,3))
+plt.plot(time.numpy(), x.numpy())
+plt.title("sin box")
+plt.xlabel("time")
+plt.ylabel("x")
+plt.show()
+
