2822186ed2be1104aaa3a60445ce4a5cb5c22b64
lmy12367 <lfuturemy@163.com>
2025-09-08 22:08:17 +0800

    dataproces

---
+++ RNN/Sequence_model/data_proces.py
@@ -0,0 +1,19 @@
+import re,collections,os,urllib.request
+
+URL='https://d2l-data.s3-accelerate.amazonaws.com/timemachine.txt'
+File='./dp/Mynet\RNN/Sequence_model/timemachine.txt'
+
+if not os.path.exists(File):
+    urllib.request.urlretrieve(URL,File)
+
+def read_time_machine():
+    with open(File,'r',encoding='utf-8') as f:
+        lines=f.readlines()
+
+    return [re.sub(r'[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+
+lines=read_time_machine()
+print("total",(len(lines)))
+print("first",lines[0])
+
+def tokenize(lines,token)
